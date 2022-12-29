6524d89c206d88a34bbf51fd5dfe78d5b29d3b07
Greg Bowler <greg.bowler@g105b.com>
2022-12-29 17:23:37 +0000

    feature: graph add nodes in constructor

---
+++ src/Graph.php
@@ -0,0 +1,18 @@
+<?php
+namespace g105b\Graph;
+
+class Graph {
+	/** @var array<Node> */
+	private array $nodeArray;
+	/** @var array<Connection> */
+	private array $connectionArray;
+
+	public function __construct(Node...$nodeArray) {
+		$this->nodeArray = $nodeArray;
+		$this->connectionArray = [];
+	}
+
+	public function hasNode(Node $node):bool {
+		return in_array($node, $this->nodeArray, true);
+	}
+}
---
+++ test/phpunit/GraphTest.php
@@ -0,0 +1,31 @@
+<?php
+namespace g105b\Graph\Test;
+
+use g105b\Graph\Graph;
+use g105b\Graph\Node;
+use PHPUnit\Framework\TestCase;
+
+class GraphTest extends TestCase {
+	public function testHasNode_no():void {
+		$node = self::createMock(Node::class);
+		$sut = new Graph();
+		self::assertFalse($sut->hasNode($node));
+	}
+
+	public function testHasNode():void {
+		$node = self::createMock(Node::class);
+		$sut = new Graph($node);
+		self::assertTrue($sut->hasNode($node));
+	}
+
+	public function testHasNode_many():void {
+		$n1 = self::createMock(Node::class);
+		$n2 = self::createMock(Node::class);
+		$n3 = self::createMock(Node::class);
+
+		$sut = new Graph($n1, $n2);
+		self::assertTrue($sut->hasNode($n1));
+		self::assertTrue($sut->hasNode($n2));
+		self::assertFalse($sut->hasNode($n3));
+	}
+}
