e529a8291e110678581a28592b08772621112776
Greg Bowler <greg.bowler@g105b.com>
2022-12-30 22:28:12 +0000

    docs: add example output

--- example/wikipedia.php
+++ example/wikipedia.php
@@ -38,3 +38,28 @@ $path = $graph->findShortestPath($nodeArray[1], $nodeArray[5], $callback);
 
 echo "Shortest path: ", implode("->", $path), PHP_EOL;
 echo "Total time: ", number_format(microtime(true) - $startTime, 6), " seconds", PHP_EOL;
+
+/*
+Example output:
+Visited Node1, connected to Node2, weight 7, distance 7 (step 0)
+Visited Node1, connected to Node3, weight 9, distance 9 (step 0)
+Visited Node1, connected to Node6, weight 14, distance 14 (step 0)
+Visited Node2, connected to Node1, weight 7, distance 14 (step 1)
+Visited Node2, connected to Node3, weight 10, distance 17 (step 1)
+Visited Node2, connected to Node4, weight 15, distance 22 (step 1)
+Visited Node3, connected to Node1, weight 9, distance 18 (step 2)
+Visited Node3, connected to Node2, weight 10, distance 19 (step 2)
+Visited Node3, connected to Node6, weight 2, distance 11 (step 2)
+Visited Node3, connected to Node4, weight 11, distance 20 (step 2)
+Visited Node6, connected to Node1, weight 14, distance 25 (step 3)
+Visited Node6, connected to Node3, weight 2, distance 13 (step 3)
+Visited Node6, connected to Node5, weight 9, distance 20 (step 3)
+Visited Node6, connected to Node1, weight 14, distance 25 (step 4)
+Visited Node6, connected to Node3, weight 2, distance 13 (step 4)
+Visited Node6, connected to Node5, weight 9, distance 20 (step 4)
+Visited Node4, connected to Node2, weight 15, distance 35 (step 5)
+Visited Node4, connected to Node3, weight 11, distance 31 (step 5)
+Visited Node4, connected to Node5, weight 6, distance 26 (step 5)
+Shortest path: Node1->Node3->Node6->Node5
+Total time: 0.000168 seconds
+*/
